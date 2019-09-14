37e81b24c8682e1c4fe9ba3de2b7675c0eedcbfe
eth212 <ethanbonn21@gmail.com>
2019-09-14 19:50:55 -0400

    updating python scripts

--- python/image_demo.py
+++ python/image_demo.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 import cv2
-import time
 import argparse
 import os
 import matplotlib
-from matplotlib import pyplot as ptl
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 import posenet
@@ -14,12 +14,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, default=101)
 parser.add_argument('--scale_factor', type=float, default=1.0)
 parser.add_argument('--notxt', action='store_true')
-parser.add_argument('--image_dir', type=str, default='./training_videos')
+parser.add_argument('--image_dir', type=str, default='./--image_dir')
 parser.add_argument('--output_dir', type=str, default='./output')
 args = parser.parse_known_args()[0]
 
 
 def main():
+    print("What side of your body are you filming? r/l")
+    side = input()
+    if side == "l":
+        side = True #filming the left side
+    else:
+        side = False #filming the right side
 
     with tf.Session() as sess:
         model_cfg, model_outputs = posenet.load_model(args.model, sess)
@@ -32,7 +38,7 @@ def main():
         filenames = [
             f.path for f in os.scandir(args.image_dir) if f.is_file() and f.path.endswith(('.png', '.jpg'))]
 
-        start = time.time()
+
         for f in filenames:
             input_image, draw_image, output_scale = posenet.read_imgfile(
                 f, scale_factor=args.scale_factor, output_stride=output_stride)
@@ -53,26 +59,99 @@ def main():
 
             keypoint_coords *= output_scale
 
+
+
+
+
+
+
+
+
+
+
+
+        if not args.notxt:
+            print("Results for image: %s" % f)
+            #manual scores
+            manual_keypoint_scores = [[]]
+            manual_keypoint_coords = [[]]
+            for pi in range(len(pose_scores)):
+                if pose_scores[pi] == 0.:
+                    break
+                print('Score = %f' % ( pose_scores[pi]))
+                manual_pose_score = [pose_scores[pi]]
+                #ki is keypoint index, s = score, c = coord
+                for ki, (s, c) in enumerate(zip(keypoint_scores[pi, :], keypoint_coords[pi, :, :])):
+
+                    print('Keypoint %s, score = %f, coord = %s' % (posenet.PART_NAMES[ki], s, c))
+                    if side == True:
+                        if (posenet.PART_NAMES[ki] == "leftEye"):
+                            Leye = (posenet.PART_NAMES[ki], s, c)
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Leye)
+                        elif (posenet.PART_NAMES[ki] == "leftShoulder"):
+                            Lshoulder = (posenet.PART_NAMES[ki], s, c)
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Lshoulder)
+                        elif (posenet.PART_NAMES[ki] == "leftHip"):
+                            Lhip = (posenet.PART_NAMES[ki], s, c)
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Lhip)
+                        elif (posenet.PART_NAMES[ki] == "leftKnee"):
+                            Lknee = (posenet.PART_NAMES[ki], s, c)
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Lknee)
+                        elif (posenet.PART_NAMES[ki] == "leftAnkle"):
+                            Lankle = (posenet.PART_NAMES[ki], s, c)
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Lankle)
+
+
+                    elif side == False:
+
+                        if (posenet.PART_NAMES[ki] == "rightEye"):
+                            Reye = (posenet.PART_NAMES[ki], s, c)
+                            Reye_score = s.tolist()
+                            Reye_coord = c.tolist()
+                            manual_keypoint_scores[pi].append(Reye_score)
+                            manual_keypoint_coords[pi].append(Reye_coord)
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Reye)
+                        elif (posenet.PART_NAMES[ki] == "rightShoulder"):
+                            Rshoulder = (posenet.PART_NAMES[ki], s, c)
+                            Rshoulder_score = s.tolist()
+                            Rshoulder_coord = c.tolist()
+                            manual_keypoint_scores[pi].append([Rshoulder_score])
+                            manual_keypoint_coords[pi].append([Rshoulder_coord])
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Rshoulder)
+                        elif (posenet.PART_NAMES[ki] == "rightHip"):
+                            Rhip = (posenet.PART_NAMES[ki], s, c)
+                            Rhip_score = s.tolist()
+                            Rhip_coord = c.tolist()
+                            manual_keypoint_scores[pi].append([Rhip_score])
+                            manual_keypoint_coords[pi].append([Rhip_coord])
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Rhip)
+                        elif (posenet.PART_NAMES[ki] == "rightKnee"):
+                            Rknee = (posenet.PART_NAMES[ki], s, c)
+                            Rknee_score = s.tolist()
+                            Rknee_coord = c.tolist()
+                            print(Rknee_coord)
+                            print(Rknee_score)
+                            manual_keypoint_scores[pi].append([Rknee_score])
+                            manual_keypoint_coords[pi].append([Rknee_coord])
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Rknee)
+                        elif (posenet.PART_NAMES[ki] == "rightAnkle"):
+                            Rankle = (posenet.PART_NAMES[ki], s, c)
+                            Rankle_score = s.tolist()
+                            Rankle_coord = c.tolist()
+                            manual_keypoint_scores[pi].append([Rankle_score])
+                            manual_keypoint_coords[pi].append([Rankle_coord])
+                            print('Collected by scrapper Keypoint %s, score = %f, coord = %s' % Rankle)
+            print(manual_keypoint_coords)
+            print(manual_keypoint_scores)
+            print(keypoint_coords)
+            print(keypoint_scores)
             if args.output_dir:
                 draw_image = posenet.draw_skel_and_kp(
                     draw_image, pose_scores, keypoint_scores, keypoint_coords,
                     min_pose_score=0.25, min_part_score=0.25)
-                print("draw_image var:")
-                print(draw_image)
 
                 cv2.imwrite(os.path.join(args.output_dir, os.path.relpath(f, args.image_dir)), draw_image)
 
-            if not args.notxt:
-                print()
-                print("Results for image: %s" % f)
-                for pi in range(len(pose_scores)):
-                    if pose_scores[pi] == 0.:
-                        break
-                    print('Pose #%d, score = %f' % (pi, pose_scores[pi]))
-                    for ki, (s, c) in enumerate(zip(keypoint_scores[pi, :], keypoint_coords[pi, :, :])):
-                        print('Keypoint %s, score = %f, coord = %s' % (posenet.PART_NAMES[ki], s, c))
-
-        print('Average FPS:', len(filenames) / (time.time() - start))
 
 
 if __name__ == "__main__":
--- python/video_handling.py
+++ python/video_handling.py
@@ -3,19 +3,19 @@ import matplotlib
 from matplotlib import pyplot
 import sys
 
-dir = 'C:/Users/Ethan/Desktop/htn2019/python/training_videos/'
+dir = 'C:/Users/Ethan/Desktop/model/posenet-python/--image_dir/'
 
 def get_video_file(vid_name, directory=dir, filetype='.mov'):
-    query = dir + vid_name + filetype
-    query = dir + "test_video.mov"
+    query = directory + vid_name + filetype
     cap = cv2.VideoCapture(query)
+    print(query)
     counter = 0
     while(True):
         counter += 1
         ret, frame = cap.read()
         if ret != False:
-            grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            grey=cv2.resize(grey, (169,300))#(width,height)
+            #grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            grey=cv2.resize(frame, (300,169))#(width,height)
             cv2.imwrite(dir + "frame" + str((counter -1 + 1000)) + '.jpg', grey)
 
         elif cv2.waitKey(25) & 0xFF == ord('q'):
@@ -23,4 +23,4 @@ def get_video_file(vid_name, directory=dir, filetype='.mov'):
         elif ret == False:
             break
 
-get_video_file(vid_name="test_video" )
+get_video_file(vid_name="optimal_squat2", directory= "C:/Users/Ethan/Desktop/htn2019/python/--image_dir/", filetype='.mp4' )
