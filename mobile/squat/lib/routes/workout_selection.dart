b9765dec241a77f5ba63e18b4e2e37884806b33d
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-14 17:09:25 -0400

    Working on adding images.

--- mobile/squat/lib/routes/workout_selection.dart
+++ mobile/squat/lib/routes/workout_selection.dart
@@ -22,22 +22,26 @@ class _WorkoutSelectionPageState extends State < WorkoutSelectionPage > {
           title: new Text('JUMPY', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300, fontSize: 45)),
           backgroundColor: SquatApp().squatPrimary
         )),
-      body: Center(
+      body: Container(child:Center(
         child: Column(
 
           // mainAxisAlignment: MainAxisAlignment.center,
           children: < Widget > [
-            Row(
-              children: < Widget > [
-                // Expanded(
-                //   child: Container(color: Color(0xff272727), child: Padding(padding: EdgeInsets.all(10), child: Text('WORKOUTS', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 28)))),
-                // ),
-              Expanded(child:Align( child:AspectRatio(child:WorkoutList(),aspectRatio: 1,)))
-              ],
-            )
+              AspectRatio(child:WorkoutList(),aspectRatio: 1,)
+            // Row(
+            //   children: < Widget > [
+            //     // Expanded(
+            //     //   child: Container(color: Color(0xff272727), child: Padding(padding: EdgeInsets.all(10), child: Text('WORKOUTS', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 28)))),
+            //     // ),
+            //   // Expanded(child:Align( child:AspectRatio(child:WorkoutList(),aspectRatio: 1,)))
+            //   AspectRatio(child:WorkoutList(),aspectRatio: 1,)
+            //   ],
+            // ),
+            
           ],
         ),
       ) // This trailing comma makes auto-formatting nicer for build methods.
+    )
     );
   }
 }
@@ -58,14 +62,14 @@ class WorkoutListState extends State<WorkoutList> {
         title: Text('WORKOUTS', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 28)),
         backgroundColor: Color(0xff272727)
       ),
-      body: buildList(),
+      body:buildList(),
     );
   }
   Widget buildList() {
-  return new ListView.builder( 
-    itemCount: someStuff.length,
-    padding: const EdgeInsets.all(10.0),
-    itemBuilder: (context, i) {
+  return new Padding(padding:EdgeInsets.all(10),child:  GridView.count( 
+    crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+      children: List.generate(3, (i) {
       print(i);
       var index = i~/2;
       // if (i.isOdd) return Divider();
@@ -76,34 +80,46 @@ class WorkoutListState extends State<WorkoutList> {
       // }
       return _buildRow(someStuff[index]);
     },
+  ),
+  ),
   );
 }
 Widget _buildRow(pair) {
   return new Center(
-    child: Card(child:
+    child: Card(
+       semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+
+          shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+          elevation: 40,
+          margin: EdgeInsets.all(10),child:
     AspectRatio(aspectRatio: 1.0,
+    
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisSize: MainAxisSize.max,
+        
         children: <Widget>[
           const ListTile(
-            leading: Icon(Icons.album),
+            
+            leading:Expanded(child: DecoratedBox(
+       decoration: BoxDecoration(
+          image: DecorationImage( 
+            image: AssetImage('assets/splash.png'),
+          ),
+        ),),),
             title: Text('The Enchanted Nightingale'),
-            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            // subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
           ),
-          ButtonTheme.bar( // make buttons use the appropriate styles for cards
-            child: ButtonBar(
-              children: <Widget>[
-                FlatButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () { /* ... */ },
-                ),
-                FlatButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () { /* ... */ },
-                ),
-              ],
-            ),
-          ),
+          // ButtonTheme.bar( // make buttons use the appropriate styles for cards
+          //   child: ButtonBar(
+          //     children: <Widget>[
+          //       // FlatButton(
+          //       //   child: const Text('BUY TICKETS'),
+          //       //   onPressed: () { /* ... */ },
+          //       // ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     )
