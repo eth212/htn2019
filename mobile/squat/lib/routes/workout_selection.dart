ac5c3a53c9204389c96a431e2a7c2be73aa1f7cd
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-14 12:40:33 -0400

    Changed background color, working on adding tiles.

--- mobile/squat/lib/routes/workout_selection.dart
+++ mobile/squat/lib/routes/workout_selection.dart
@@ -2,38 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:squat/main.dart';
 
 class WorkoutSelectionPage extends StatefulWidget {
-  WorkoutSelectionPage({Key key, this.title}) : super(key: key);
+  WorkoutSelectionPage({
+    Key key,
+    this.title
+  }): super(key: key);
   final String title;
   @override
   _WorkoutSelectionPageState createState() => _WorkoutSelectionPageState();
 }
 
-class _WorkoutSelectionPageState extends State<WorkoutSelectionPage> {
+class _WorkoutSelectionPageState extends State < WorkoutSelectionPage > {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-              appBar: AppBar( 
-        centerTitle: true,
-        title: new Text('JUMPY',style: TextStyle(color:Colors.white,fontWeight: FontWeight.w300, fontSize: 45)),
-        backgroundColor: SquatApp().squatPrimary
+      backgroundColor: Color(0xff272727),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0), child: AppBar(
+          centerTitle: true,
+          title: new Text('JUMPY', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300, fontSize: 45)),
+          backgroundColor: SquatApp().squatPrimary
+        )),
+      body: Center(
+        child: Column(
+
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: < Widget > [
+            Row(
+              children: < Widget > [
+                Expanded(
+                  child: Container(color: Color(0xff272727), child: Padding(padding: EdgeInsets.all(10), child: Text('WORKOUTS', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 28)))),
+                ),
+                Expanded(
+                   child: Container(color: Colors.blue),
+                )
+              ],
+            )
+          ],
         ),
-        body: Center(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(color: Colors.blue,child:Text('WORKOUTS',style: TextStyle(color:Colors.white,fontWeight: FontWeight.w300, fontSize: 45))),
-                  ),
-                  // Expanded(
-                  //   child: Container(color: Colors.blue),
-                  // )
-                ],
-              )
-            ],
-          ),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
