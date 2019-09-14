961920263cb27cf0ba445b939b6c8a811d2d11c0
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-14 12:24:19 -0400

    Adding top text.

--- mobile/squat/lib/routes/workout_selection.dart
+++ mobile/squat/lib/routes/workout_selection.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:squat/main.dart';
 
 class WorkoutSelectionPage extends StatefulWidget {
   WorkoutSelectionPage({Key key, this.title}) : super(key: key);
@@ -11,21 +12,23 @@ class _WorkoutSelectionPageState extends State<WorkoutSelectionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+              appBar: AppBar( 
+        centerTitle: true,
+        title: new Text('JUMPY',style: TextStyle(color:Colors.white,fontWeight: FontWeight.w300, fontSize: 45)),
+        backgroundColor: SquatApp().squatPrimary
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Row(
                 children: <Widget>[
                   Expanded(
-                    child: Container(color: Colors.blue),
+                    child: Container(color: Colors.blue,child:Text('WORKOUTS',style: TextStyle(color:Colors.white,fontWeight: FontWeight.w300, fontSize: 45))),
                   ),
-                  Expanded(
-                    child: Container(color: Colors.blue),
-                  )
+                  // Expanded(
+                  //   child: Container(color: Colors.blue),
+                  // )
                 ],
               )
             ],
