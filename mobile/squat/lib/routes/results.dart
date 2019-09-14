fd35de8ad18d97ebe7559179b8ef4545ac0aa149
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-14 10:39:39 -0400

    Changed SDK version, added page.

---
+++ mobile/squat/lib/routes/results.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatefulWidget {
+  ResultsPage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  ResultsPageState createState() => ResultsPageState();
+}
+
+class ResultsPageState extends State<ResultsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.white60, const Color(0xff6A8ADB)]),
+          ),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(color: Colors.blue),
+              ),
+              Spacer(flex: 2),
+            ],
+          )),
+    );
+  }
+}
